2c7ecbbb145ef75fdbde87fda675f6fa590702a4
Mahimur Rahman Khan <mahimrk.a@gmail.com>
2025-01-30 02:21:38 -0500

    hamburger implemented

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-<script>
-  // Select hamburger and nav links
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
 
-  // Toggle menu on hamburger click
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
-  });
-</script>
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  // Toggle the 'active' class on the hamburger and nav-links
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
